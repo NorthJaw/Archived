db1ee990bf6adfa63396de1409b10325352f2cbf
BatMan <venkatpraveenx@gmail.com>
2018-04-20 14:20:17 +0530

    s

--- makepayment.php
+++ makepayment.php
@@ -23,7 +23,7 @@ foreach ($rows as $row) {
 
     $db->query("UPDATE `alpr_data` SET `wallet` = ".$wallet." WHERE `id` = ".$_SESSION['id']);
     $db->query("INSERT INTO history(userid,toll_name,toll_charge) VALUES (".$id.",".$tg_name.",".$tg_charge.")");
-
+    $db->query("DELETE FROM `requests` WHERE `userid` =".$_SESSION['id']);
 
     header('Location: Home.php')
 ?>
